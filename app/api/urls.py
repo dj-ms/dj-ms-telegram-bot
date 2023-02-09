11dd9783407488d93be492374f581283ee88c6cf
Рустам Астафеев <astafeev0308@gmail.com>
2023-02-09 20:50:30 +0500

    Initial commit

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
+from app.api.views import telegram_views
 
 urlpatterns = [
-
+    # path('__debug__/', include(debug_toolbar.urls)),
+    path('super_secter_webhook/', csrf_exempt(telegram_views.TelegramBotWebhookView.as_view())),
 ]
